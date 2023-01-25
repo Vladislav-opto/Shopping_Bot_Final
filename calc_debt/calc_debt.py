3b3c0ff51a3dddf610a6d50f07cf22def2c8fe49
Alexander Levkin <49125444+Comandosss@users.noreply.github.com>
2023-01-25 18:15:16 +0300

    Исправили функцию подсчета количества выбранных категорий пользователями и привязку выбранных категорий к пользователю

--- calc_debt/calc_debt.py
+++ calc_debt/calc_debt.py
@@ -4,22 +4,22 @@ from database.db import db_session
 
 
 def calc_number_of_participants_for_receipt(receipt_id: int) -> int|None:
-    # Подсчет количества участников вечеринки #
+    """Подсчет количества участников вечеринки"""
     set_of_users = set()
     try:
-        filtered_by_receipt_id = db_session.query(CategoryByUser).filter(CategoryByUser.receipt_id == receipt_id).all() #список id записей по данному чеку
+        filtered_by_receipt_id = db_session.query(CategoryByUser).filter(CategoryByUser.receipt_id == receipt_id).all()
         for line in filtered_by_receipt_id:
-            set_of_users.add(line.user_id) #формирование множества user_id для конкретного чека
+            set_of_users.add(line.user_id)
         return len(set_of_users)
     except:
         return None    
 
 
 def calc_sum_of_categories(receipt_id: int) -> dict|None:
-    # Формирование словаря - категория: сумма #
+    """Формирование словаря - категория: сумма """
     dict_sum_categories = {}
     try:
-        filtered_by_receipt_id = db_session.query(Good).filter(Good.receipt_id == receipt_id).all() #список id записей по данному чеку
+        filtered_by_receipt_id = db_session.query(Good).filter(Good.receipt_id == receipt_id).all()
         for line in filtered_by_receipt_id:
             if line.category_id in dict_sum_categories:
                 dict_sum_categories[line.category_id] += line.sum
@@ -30,59 +30,64 @@ def calc_sum_of_categories(receipt_id: int) -> dict|None:
         return None
 
 
-def create_dict_category_quantuty_users(receipt_id: int, category_list: list) -> dict|None: # Список из веба достаем
+def create_dict_category_quantuty_users(receipt_id: int, category_list: list) -> dict|None:
+    """Подсчет количества выбраных категорий пользователями"""
     quantity_entry = calc_number_of_participants_for_receipt(receipt_id)
     dict_category_quantity_users = {}
     try:
         filtered_by_receipt_id = db_session.query(CategoryByUser).filter(CategoryByUser.receipt_id == receipt_id).all() #список id записей по данному чеку
         for category in category_list:
+            counter = 0
             quantity_entry_in_func = quantity_entry
             for line in filtered_by_receipt_id:
                 if category == line.category_id:
                     quantity_entry_in_func -= 1
-            dict_category_quantity_users[category] = quantity_entry_in_func
+                    counter += 1
+                elif line.category_id == 0:
+                    counter += 1
+            dict_category_quantity_users[category] = counter
         return dict_category_quantity_users
     except:
         return None
 
 
 def create_dict_user_categories(receipt_id: int, category_list: list) -> dict[list]|None:
+    """"Привязка выбранных категорий к пользователю"""
     dict_user_categories = {}
     list_of_user_ids = []
+    temp_category_list = []
     try:
         filtered_by_receipt_id = db_session.query(CategoryByUser).filter(CategoryByUser.receipt_id == receipt_id).all() #список id записей по данному чеку
         for line in filtered_by_receipt_id:
+            if list_of_user_ids:
+                if line.user_id != list_of_user_ids[-1]:
+                    temp_category_list = []
             if line.category_id == 0:
                 dict_user_categories[line.user_id] = category_list
-                break
-            temp_category_list = [
-                category for category in category_list
-            ]
+                continue
             if line.user_id in list_of_user_ids:
-                current_list = dict_user_categories[line.user_id]
-                current_list.remove(line.category_id)
-                dict_user_categories[line.user_id] = current_list
+                temp_category_list.append(line.category_id)
+                dict_user_categories[line.user_id] = temp_category_list
             else:
                 list_of_user_ids.append(line.user_id)
-                temp_category_list.remove(line.category_id)
+                temp_category_list.append(line.category_id)
                 dict_user_categories[line.user_id] = temp_category_list
         return dict_user_categories
     except:
         return None
 
 
-def calculate_user_debt(receipt_id: int, category_list: list) -> list|None: #расчет задолженности конкретного человека
+def calculate_user_debt(receipt_id: int, category_list: list) -> list|None:
+    """Расчет задолженности конкретного человека"""
     try:
         dict_user = create_dict_user_categories(receipt_id, category_list)
         dict_user_debt = {}
-        list_users_debt = []
         for user_id in dict_user:
             summ = 0
             for category_id in dict_user[user_id]:
                 summ += calc_sum_of_categories(receipt_id)[category_id]/create_dict_category_quantuty_users(receipt_id, category_list)[category_id]
             user = db_session.query(AuthWebApp).filter(AuthWebApp.id == user_id).first()
             dict_user_debt[user.last_name] = "{:.2f}".format(summ)
-        print(dict_user_debt)
         return dict_user_debt
     except:
         return None
